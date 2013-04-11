5e1c2e40882875fd742e177968e82d420aecf367
George Lee <keoki.lee@gmail.com>
2013-04-10 16:50:43 -1000

    Removed body parameter from requests.

--- bomberman/client.py
+++ bomberman/client.py
@@ -5,13 +5,13 @@ from bomberman.error import Unauthorized
 from bomberman.error import InternalServerError
 from bomberman.connection import Connection
 
-class Client(object): 
+class Client(object):
   def __init__(self):
     self.conn = Connection()
 
   def is_profane(self, corpus):
     params = urllib.urlencode({'corpus': corpus})
-    self.conn.request("GET", "/v1/profanity/check?%s" % params, {}, self.conn.headers)
+    self.conn.request("GET", "/v1/profanity/check?%s" % params, headers=self.conn.headers)
     resp = self.conn.getresponse()
     if resp.status == 200:
       profane = resp.read()
@@ -23,7 +23,7 @@ class Client(object):
 
   def censor(self, corpus, replacement_text="***"):
     params = urllib.urlencode({'corpus': corpus, 'replacement_text': replacement_text})
-    self.conn.request("GET", "/v1/profanity/censor?%s" % params, {}, self.conn.headers)
+    self.conn.request("GET", "/v1/profanity/censor?%s" % params, headers=self.conn.headers)
     resp = self.conn.getresponse()
     if resp.status == 200:
       data = json.loads(resp.read())
@@ -32,10 +32,10 @@ class Client(object):
     else:
       self.conn.close()
       self.__raise_exception(resp.status)
-    
+
   def highlight(self, corpus, start_tag="<strong>", end_tag="</strong>"):
     params = urllib.urlencode({'corpus': corpus, 'start_tag': start_tag, 'end_tag': end_tag})
-    self.conn.request("GET", "/v1/profanity/highlight?%s" % params, {}, self.conn.headers)
+    self.conn.request("GET", "/v1/profanity/highlight?%s" % params, headers=self.conn.headers)
     resp = self.conn.getresponse()
     if resp.status == 200:
       data = json.loads(resp.read())
@@ -44,7 +44,7 @@ class Client(object):
     else:
       self.conn.close()
       self.__raise_exception(resp.status)
-      
+
   def __raise_exception(self, code):
     if code == 400:
       raise BadRequest
@@ -54,4 +54,3 @@ class Client(object):
       raise InternalServerError
     else:
       raise Exception("Bomberman returned error code %s" % code)
-    
